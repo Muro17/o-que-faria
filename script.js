5ea06e3e16cbe0bbd625995f8312aa196b53f9df
Muro <105938976+Muro17@users.noreply.github.com>
2024-08-16 11:23:46 -0300

    HGGG

---
+++ script.js
@@ -0,0 +1,8 @@
+const CaixaPrincipal = document.querySelector ('caixa-principal');
+const CaixaPerguntas = document.querySelector ('caixa-perguntas');
+const CaixaAlternativas = document.querySelector ('caixa-alternativas');
+const CaixaResultado = document.querySelector ('caixa-resultado');
+const CaixaResultado = document.querySelector ('caixa-resultado');
+
+let atual = 0;
+let perguntasAtual
