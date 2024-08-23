caeca2752b63816eca1f4e6953d6f9c152743041
Muro <105938976+Muro17@users.noreply.github.com>
2024-08-23 11:22:54 -0300

    Update shshs

--- script.js
+++ script.js
@@ -1,73 +1,143 @@
-const CaixaPrincipal = document.querySelector ('.caixa-principal');
-const CaixaPerguntas = document.querySelector ('.caixa-perguntas');
-const CaixaAlternativas = document.querySelector ('.caixa-alternativas');
-const CaixaResultado = document.querySelector ('.caixa-resultado');
-const textoResultado = document.querySelector ('.texto-resultado');
+const caixaPrincipal = document.querySelector( '.caixa-principal');
+
+const caixaPerguntas = document.querySelector( '.caixa-perguntas');
+
+const caixaAlternativas = document.querySelector( '.caixa-alternativas');
+
+const caixaResultado = document.querySelector( '.caixa-resultado');
+
+const textoResultado = document.querySelector( '.texto-resultado');
+
+
 
 const perguntas = [
+
     {
-        enunciado: "No ambito social, vc prefere:",
+
+        enunciado: "No Âmbito social, você prefere:",
+
         alternativas: [
+
             {
-                texto:"alternativas 1",
-                afirmaçao: "afirmaçao",
-            }, 
-            {
-                texto: "alternativa 2",
-                afirmaçao: "afirmaçao",
+
+                texto: "Que a corrupção acabe, mas seus braços viram suas pernas e vice-versa.",
+
+                afirmacao: "afirmação",
+
             },
+
+            {
+
+                texto: "Que a desigualdade monetária acabasse, mas você nunca mais poderá usar qualquer tipo de vestimenta.",
+
+                afirmacao: "afirmação",
+
+            },
+
         ]
+
     },
+
     {
-        enunciado: "No ambito ambiental, vc prefere:",
+
+        enunciado: "No Âmbito ambiental, você prefere:",
+
         alternativas: [
+
             {
-                texto:"alternativas 3",
-                afirmaçao: "afirmaçao",
-            }, 
-            {
-                texto: "alternativa 4",
-                afirmaçao: "afirmaçao",
+
+                texto: "Acabar com o desmatamento, mas você só poderá comer dejetos de animais. ",
+
+                afirmacao: "afirmação",
+
             },
+
+            {
+
+                texto: "Que o tráfico de animais acabasse, mas você nunca mais poderá fazer sexo e nem qualquer tipo de prazer.  ",
+
+                afirmacao: "afirmação",
+
+            },
+
         ]
+
     },
+
     {
-        enunciado: "No ambito tecnologico, vc prefere:",
+
+        enunciado: "No Âmbito tecnológico, você prefere:",
+
         alternativas: [
+
             {
-                texto:"alternativas 5",
-                afirmaçao: "afirmaçao",
-            }, 
-            {
-                texto: "alternativa 6",
-                afirmaçao: "afirmaçao",
+
+                texto: "Que os carros voadores existam, mas todo mundo irá vomitar em você quando te vissem.",
+
+                afirmacao: "afirmação",
+
             },
+
+            {
+
+                texto: "Criar a invenção dos seus sonhos, mas no mês seguinte começará uma guerra nuclear que afetaráo mundo inteiro.",
+
+                afirmacao: "afirmação",
+
+            },
+
         ]
+
     },
+
 ]
 
 
+
 let atual = 0;
-let perguntasAtual;
-    
-    function mostraPergunta () {
-    perguntasAtual = perguntas[atual];
-    CaixaPerguntas.textContent = perguntasAtual.enunciado;
-    CaixaAlternativas.textContent="";
+
+let perguntaAtual;
+
+
+
+function mostraPergunta (){
+
+    perguntaAtual = perguntas[atual];
+
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+
+    caixaAlternativas.textContent="";
+
     mostraAlternativa();
+
 }
-   
-    function mostraAlternativa (){
-        for (const pergunta of perguntasAtual.alternativas){
-             const botaoAlternativa = document.createrElement('button');
-            botaoAlternativa.textContent = pergunta.texto;
-            botaoAlternativa.addEventListener ("click") , functiona(){
-                atual++;
-                
-            }
-            CaixaAlternativas.appendChild(botaoAlternativa);
-            mostraAlternativa();
-        }
+
+
+
+function mostraAlternativa () {
+
+    for (const pergunta of perguntaAtual.alternativas) {
+
+        const botaoAlternativa = document.createElement('button');
+
+        botaoAlternativa.textContent= pergunta.texto;
+
+        botaoAlternativa.addEventListener("click", function(){
+
+            atual++;
+
+            mostraPergunta();
+
+        })
+
+        caixaAlternativas.appendChild(botaoAlternativa);
+
+    
+
+    }
+
 }
+
+
 
 mostraPergunta();
